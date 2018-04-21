57aef59b835f87cfe2bb515975e603b7207471f4
Ben <menubar@protonmail.com>
2018-04-21 07:37:41 -0400

    new

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import S3Dropzone from './S3Dropzone'
 import Thumbnail from './components/Thumbnail'
-import Button from './components/Button'
 import SpinnerComponent from './components/Thumbnail/SpinnerComponent'
 import BaseDropzone from './components/BaseDropzone'
 
 exports.S3Dropzone = S3Dropzone
-exports.Button = Button
 exports.Thumbnail = Thumbnail
 exports.SpinnerComponent = SpinnerComponent
 exports.BaseDropzone = BaseDropzone
 
 export default {
   S3Dropzone,
-  Button,
   Thumbnail,
   SpinnerComponent,
   BaseDropzone
